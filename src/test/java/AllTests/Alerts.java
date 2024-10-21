5145098eb387e97421d300df820a0aadebeec0fe
Shakib-ewu <hossainsakib58@gmail.com>
2024-10-21 21:40:05 +0600

    new testcase added

---
+++ src/test/java/AllTests/Alerts.java
@@ -0,0 +1,24 @@
+package AllTests;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) throws InterruptedException {
+		 ChromeDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();	
+	        driver.get("https://testautomationpractice.blogspot.com/");
+	        
+	        driver.findElement(By.id("alertBtn")).click();
+	        Thread.sleep(3000);
+	        
+	        Alert alert=driver.switchTo().alert();
+	        alert.accept();
+	        driver.quit();
+	        
+
+	}
+
+}
