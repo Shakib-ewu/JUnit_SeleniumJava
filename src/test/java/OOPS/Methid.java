1eec664aa183195abeed723678b2ee3fec4c20d8
Shakib-ewu <hossainsakib58@gmail.com>
2024-11-04 13:04:39 +0600

    new tests added

---
+++ src/test/java/OOPS/Methid.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class Methid {
+
+	
+		static void myMethod() {
+		    System.out.println("I just got executed!");
+		  }
+
+		  public static void main(String[] args) {
+		    myMethod();
+		    myMethod();
+		    myMethod();
+		  }
+
+	}
+	
+
+
